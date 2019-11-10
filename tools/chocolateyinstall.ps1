09993890993b116ff587ffd52d2bef2b3814dc76
Rafal Zawadzki <bluszcz@bluszcz.net>
2019-11-10 21:53:24 +0100

    0.8 Version working

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,8 +4,9 @@ $handbook = Join-Path (Split-Path $tools) 'tools/DeerPortal-0.8.2/data/assets/pd
 $classic = Join-Path $content 'DeerPortal.exe'
 $classic_shortcut = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'Deer Portal Game.lnk'
 
+Get-ChocolateyWebFile -PackageName 'deerportal' -FileFullPath "$tools\DeerPortal-0.8.2.zip" -Url 'https://github.com/deerportal/deerportal/releases/download/v0.8.2/Windows-DeerPortal-0.8.2.zip'
 
-Install-ChocolateyZipPackage 'deerportal' '.\DeerPortal-0.8.2.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'deerportal' "$tools\DeerPortal-0.8.2.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyShortcut `
     -ShortcutFilePath $classic_shortcut `
